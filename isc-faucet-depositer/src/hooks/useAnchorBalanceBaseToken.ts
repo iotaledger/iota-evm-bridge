cd116e0f7227f7165e0df03f8dd2cd1686ea19a5
Branko <brankobosnic1@gmail.com>
2025-03-27 10:19:33 +0100

    fix: hook params

--- isc-faucet-depositer/src/hooks/useAnchorBalanceBaseToken.ts
+++ isc-faucet-depositer/src/hooks/useAnchorBalanceBaseToken.ts
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { useEvmRpcClientContext } from '../contexts';
 
-export function useAnchorBalanceBaseToken(address: string, chainId: string) {
+export function useAnchorBalanceBaseToken(address: string) {
     const { evmRpcClient } = useEvmRpcClientContext();
 
     return useQuery({
-        queryKey: ['anchor-balance-base-token', address, chainId, evmRpcClient?.baseUrl],
+        queryKey: ['anchor-balance-base-token', address, evmRpcClient?.baseUrl],
         queryFn: async () => {
             if (!evmRpcClient?.baseUrl) return {};
 
-            return await evmRpcClient.getBalanceBaseToken(address, chainId);
+            return await evmRpcClient.getBalanceBaseToken(address);
         },
         enabled: !!address,
         staleTime: 1000 * 60 * 5,
