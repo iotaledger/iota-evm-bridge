82f5c1e8b5c704c679d05702464367dc29f81110
Mario Sarcevic <mario.sarcevic@iota.org>
2025-04-14 11:14:44 +0200

    Disable parallel tests and only 1 retry

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     testDir: './tests',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 2 : undefined,
+    retries: process.env.CI ? 1 : 0,
+    workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     expect: {
         timeout: 10_000,
