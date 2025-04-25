d6455ded4caac0feaadb922ba2d5c9ddd421b2e5
marc2332 <mespinsanz@gmail.com>
2025-04-25 17:45:03 +0200

    chore: Fix rounding

--- app/tests/depositThenWithdraw.spec.ts
+++ app/tests/depositThenWithdraw.spec.ts
@@ -94,7 +94,7 @@ test.describe.serial('Deposit then withdraw roundtrip', () => {
             .locator('xpath=../div/span')
             .nth(1)
             .textContent();
-        expect(gasFeeValue).toEqual('0.0066316');
+        expect(gasFeeValue).toEqual('0.006631');
 
         const youReceiveValue = await pageWithL1Wallet
             .locator('div:has(> span:text("You Receive"))')
@@ -162,7 +162,7 @@ test.describe.serial('Deposit then withdraw roundtrip', () => {
             .locator('xpath=../div/span')
             .nth(1)
             .textContent();
-        expect(Number(gasFeeValue).toFixed(6)).toEqual('0.00003801');
+        expect(Number(gasFeeValue).toFixed(6)).toEqual('0.000038');
 
         const youReceiveValue = await pageWithL2Wallet
             .locator('div:has(> span:text("You Receive"))')
