0e05ec51be0a3c86029b376bc818bcdd5a01f1a0
marc2332 <mespinsanz@gmail.com>
2025-04-30 11:58:45 +0200

    lower minimum to send

--- app/src/lib/schema/bridgeForm.schema.ts
+++ app/src/lib/schema/bridgeForm.schema.ts
@@ -17,7 +17,7 @@ export function createBridgeFormSchema(
                 .trim()
                 .refine(
                     (value) => {
-                        return new BigNumber(value).isGreaterThanOrEqualTo(1);
+                        return new BigNumber(value).isGreaterThanOrEqualTo(0.0001);
                     },
                     {
                         message: 'Invalid amount',
